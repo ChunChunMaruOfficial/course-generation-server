a5022c39f58864ed9e3474bc0a809e76b2d644a0
ChunChunMaruOfficial <c4erkaoui@gmail.com>
2025-11-29 17:54:11 +0300

    222

--- postmethods/generateExplanation.js
+++ postmethods/generateExplanation.js
@@ -11,17 +11,17 @@ async function generateExplanation  (req, res) {
      if (req.body.userid) {
         //ПОД ЗАМЕНУ!!!!!!
             users.find(v => v.id === req.body.userid).courses.find(v => v.id === req.body.courseid).modules.find(v => v.id === req.body.moduleid).lessons.push(result)
-        } else {
-            //  courses.find(v => v.id === req.body.courseid).modules.find(v => v.id === req.body.moduleid).lessons.push(result)
-            const course = courses.find(v => v.id === req.body.courseid);
-            const module = course?.modules.find(v => v.id === req.body.moduleid);
-            const lesson = module?.lessons.find(v => v.id === req.body.lessonid);
+        } 
+        // else {
+        //     const course = courses.find(v => v.id === req.body.courseid);
+        //     const module = course?.modules.find(v => v.id === req.body.moduleid);
+        //     const lesson = module?.lessons.find(v => v.id === req.body.lessonid);
     
-            if (lesson) {
-                lesson.selectedwords = result;
-                fs.writeFileSync('data/courses.json', JSON.stringify(courses, null, 2), 'utf-8');
-            }
-        }
+        //     if (lesson) {
+        //         lesson.selectedwords = result;
+        //         fs.writeFileSync('data/courses.json', JSON.stringify(courses, null, 2), 'utf-8');
+        //     }
+        // }
     res.json({ result });
 }
 
