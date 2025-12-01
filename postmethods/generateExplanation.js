ce2b2e16a127ad8aa439a7de79d66ffd29dd3c7a
ChunChunMaruOfficial <c4erkaoui@gmail.com>
2025-12-02 01:30:16 +0300

    fortuna

--- postmethods/generateExplanation.js
+++ postmethods/generateExplanation.js
@@ -2,27 +2,22 @@ const prompts = require('../data/shortcourse.json');
 const handleGenerate = require('./handleGenerate')
 const fs = require('fs');
 
-async function generateExplanation  (req, res) {
-    let courses = JSON.parse(fs.readFileSync('data/courses.json', 'utf-8'));
-    
-    const result = await handleGenerate(prompts.newwordpromt + req.body.topic);
-    
-    lesson.explanations.push(result)
-     if (req.body.userid) {
-        //ПОД ЗАМЕНУ!!!!!!
-            users.find(v => v.id === req.body.userid).courses.find(v => v.id === req.body.courseid).modules.find(v => v.id === req.body.moduleid).lessons.push(result)
-        } 
-        // else {
-        //     const course = courses.find(v => v.id === req.body.courseid);
-        //     const module = course?.modules.find(v => v.id === req.body.moduleid);
-        //     const lesson = module?.lessons.find(v => v.id === req.body.lessonid);
-    
-        //     if (lesson) {
-        //         lesson.selectedwords = result;
-        //         fs.writeFileSync('data/courses.json', JSON.stringify(courses, null, 2), 'utf-8');
-        //     }
-        // }
-    res.json({ result });
+async function generateExplanation(req, res) {
+    const users = JSON.parse(fs.readFileSync('data/users.json', 'utf-8'));
+    let guestsessions = JSON.parse(fs.readFileSync('data/guestsessions.json', 'utf-8'));
+
+    console.log(req.body);
+    const result = await handleGenerate(prompts.newwordpromt.replace('{{topic}}', req.body.topic).replace('{{context}}', req.body.context));
+
+    if (req.body.userid) {
+        users.find(v => v.id === req.body.userid).cuorses.find(v => v.id === req.body.courseid).modules.find(v => v.id === req.body.moduleid).lessons.find(v => v.id === req.body.lessonid).selectedwords.push({ word: req.body.topic, explanation: result })
+    }
+    else {
+        guestsessions.find(v => v.id === req.body.guestId).courses.find(v => v.id === req.body.courseid).modules.find(v => v.id === req.body.moduleid).lessons.find(v => v.id === req.body.lessonid).selectedwords.push({ word: req.body.topic, explanation: result })
+        fs.writeFileSync('data/guestsessions.json', JSON.stringify(guestsessions, null, 2), 'utf-8');
+    }
+     
+    res.json({ explanation: result });
 }
 
 module.exports = generateExplanation;
--- postmethods/generateLesson.js
+++ postmethods/generateLesson.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 async function generateLesson(req, res) {
 let courses = JSON.parse(fs.readFileSync('data/courses.json', 'utf-8'));
 const users = JSON.parse(fs.readFileSync('data/users.json', 'utf-8'));
+let guestsessions = JSON.parse(fs.readFileSync('data/guestsessions.json', 'utf-8'));
 
 
     const prompt = prompts.lesson.replace('{{lesson_name}}', req.body.topic).replace('{{course_structure}}', req.body.course_structure).replace('{{context}}', req.body.context ?? 'This information can be omitted.')
@@ -17,7 +18,9 @@ const users = JSON.parse(fs.readFileSync('data/users.json', 'utf-8'));
     console.log(result);
     
     if (req.body.userid) {
-        users.find(v => v.id === req.body.userid).courses.find(v => v.id === req.body.courseid).modules.find(v => v.id === req.body.moduleid).lessons.find(v => v.id === req.body.lessonid).content = result.lesson_text
+       users.find(v => v.id === req.body.userid).courses.find(v => v.id === req.body.courseid).modules.find(v => v.id === req.body.moduleid).lessons.find(v => v.id === req.body.lessonid).content = result.lesson_text
+       users.find(v => v.id === req.body.userid).courses.find(v => v.id === req.body.courseid).modules.find(v => v.id === req.body.moduleid).lessons.find(v => v.id === req.body.lessonid).links = result.links
+       fs.writeFileSync('data/users.json', JSON.stringify(users, null, 2), 'utf-8');
     } else {
         const course = courses.find(v => v.id === req.body.courseid);
         const module = course?.modules.find(v => v.id === req.body.moduleid);
@@ -27,6 +30,10 @@ const users = JSON.parse(fs.readFileSync('data/users.json', 'utf-8'));
             lesson.content = result.lesson_text;
             lesson.links = result.links;
             fs.writeFileSync('data/courses.json', JSON.stringify(courses, null, 2), 'utf-8');
+
+            guestsessions.find(v => v.id === req.body.guestId).courses.find(v => v.id === req.body.courseid).modules.find(v => v.id === req.body.moduleid).lessons.find(v => v.id === req.body.lessonid).content = result.lesson_text
+            guestsessions.find(v => v.id === req.body.guestId).courses.find(v => v.id === req.body.courseid).modules.find(v => v.id === req.body.moduleid).lessons.find(v => v.id === req.body.lessonid).links = result.links
+            fs.writeFileSync('data/guestsessions.json', JSON.stringify(guestsessions, null, 2), 'utf-8');
         }
 
     }
